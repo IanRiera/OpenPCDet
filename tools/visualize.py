d70cf014b37b57b4a4082a09019da60d51aa0635
Ian Riera <ian.riera.smolinska@gmail.com>
2021-05-19 12:37:42 +0200

    visualization_tools

---
+++ tools/visualize.py
@@ -0,0 +1,78 @@
+import argparse
+import glob
+from pathlib import Path
+from plyfile import PlyData
+
+import mayavi.mlab as mlab
+import numpy as np
+
+import sys
+sys.path.insert(1, 'D:\\Ian\\UNI\\5_Master_CV\\M9_TFM\\repo')
+
+from OpenPCDet.pcdet.config import cfg, cfg_from_yaml_file
+from OpenPCDet.pcdet.utils import common_utils
+from OpenPCDet.tools.visual_utils import visualize_utils as V
+
+from functools import partial
+
+import pickle
+from copy import deepcopy
+
+def parse_config():
+    parser = argparse.ArgumentParser(description='arg parser')
+    parser.add_argument('--model', type=str, default='pv_rcnn', help='model used to predict the boxes')
+    parser.add_argument('--score', type=float, default=0.0, help='confidence threshold to show detections')
+    parser.add_argument('--ped', action='store_true', help='only show pedestrian detections')
+    parser.add_argument('--points_path',  type=str, default='D:\\Ian\\UNI\\5_Master_CV\\M9_TFM\\media\\kitti\\training\\velodyne\\000008.bin')
+    parser.add_argument('--detection_path', type=str, default='D:\\Ian\\UNI\\5_Master_CV\\M9_TFM\\results\\openpcdet\\0_demo\\000008_pred_pv_rcnn.pkl')
+
+    args = parser.parse_args()
+
+    return args
+
+def read_ply(ply_path):
+    plydata = PlyData.read(ply_path)
+    vertex = plydata.elements[0].data
+    points = np.asarray(vertex.tolist())
+    return points
+
+def main():
+    args = parse_config()
+    logger = common_utils.create_logger()
+
+    points = np.fromfile(args.points_path,
+                            dtype=np.float32).reshape(-1, 4)
+    
+    with open(args.detection_path,
+                'rb') as f:
+        pred = pickle.load(f)
+    #p = pred['../pointclouds_beamagine/kitti_' + args.sample + '.bin']
+
+
+    filtered_boxes = []
+    filtered_scores = []
+    filtered_labels = []
+
+    p = list(pred.values())[0]
+    for idx, score in enumerate(p['scores']):
+        if not args.ped or args.ped and p['labels'][idx] == 2:
+            if score >= args.score:
+                filtered_boxes.append(p['boxes'][idx])
+                filtered_scores.append(p['scores'][idx])
+                filtered_labels.append(p['labels'][idx])
+
+                if p['labels'][idx] == 2:
+                    print(p['boxes'][idx])
+
+    V.draw_scenes(
+        points=points, ref_boxes=np.array(filtered_boxes),
+        ref_scores=np.array(filtered_scores), ref_labels=np.array(filtered_labels)
+    )
+
+    mlab.show(stop=True)
+
+    logger.info('Demo done.')
+
+
+if __name__ == '__main__':
+    main()
