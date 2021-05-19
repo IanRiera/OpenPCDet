bcb6c93d56167d6dff27bc5f119df4615fc5f499
Ian Riera <ian.riera.smolinska@gmail.com>
2021-05-19 13:59:32 +0200

    Update visualize.py

--- tools/visualize.py
+++ tools/visualize.py
@@ -21,7 +21,7 @@ from copy import deepcopy
 def parse_config():
     parser = argparse.ArgumentParser(description='arg parser')
     parser.add_argument('--model', type=str, default='pv_rcnn', help='model used to predict the boxes')
-    parser.add_argument('--score', type=float, default=0.0, help='confidence threshold to show detections')
+    parser.add_argument('--score', type=float, default=0.6, help='confidence threshold to show detections')
     parser.add_argument('--ped', action='store_true', help='only show pedestrian detections')
     parser.add_argument('--points_path',  type=str, default='D:\\Ian\\UNI\\5_Master_CV\\M9_TFM\\media\\kitti\\training\\velodyne\\000008.bin')
     parser.add_argument('--detection_path', type=str, default='D:\\Ian\\UNI\\5_Master_CV\\M9_TFM\\results\\openpcdet\\0_demo\\000008_pred_pv_rcnn.pkl')
